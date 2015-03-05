714e4e728060250852bc0d1eb470bdaa02d6d70b
Tianhe Yang <tianhe.yang@gmail.com>
2015-03-04 18:54:49 -0800

    Added mobile navigation

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         concat: {
             js: {
                 src: [
-                    'js/angular.min.js','js/angular-route.min.js','js/angular-animate.min.js','node_modules/angular-loading-bar/build/loading-bar.min.js','js/angular-touch.min.js','js/angular-slider.min.js','bower_components/angular-snap/angular-snap.min.js','js/controllers.js','js/twitter-fetcher.js','bower_components/oauth-js/dist/oauth.js'
+                    'js/angular.min.js','js/angular-route.min.js','js/angular-animate.min.js','node_modules/angular-loading-bar/build/loading-bar.min.js','js/angular-touch.min.js','js/angular-slider.min.js','js/snap.js','bower_components/angular-snap/angular-snap.min.js','js/controllers.js','js/twitter-fetcher.js','bower_components/oauth-js/dist/oauth.js'
                 ],
                 dest: 'bin/grunt-combined.js'
             }
